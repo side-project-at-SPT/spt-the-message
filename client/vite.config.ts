47f12bc6511ba7699603c3b9a1a8248c97ede6f8
sin_yu <catmoon1017@gmail.com>
2024-12-21 11:55:21 +0800

    feat: axions config, vite proxy config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const { VITE_API_URL, VITE_PRODUCT_URL } = env
+
+  return {
+    server: {
+      proxy: {
+        [`${VITE_API_URL}`]: {
+          target: VITE_PRODUCT_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(`/${VITE_API_URL}`, '')
+        }
+      }
+    },
+    define: {
+      _VERSION_: JSON.stringify(process.env.npm_package_version)
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
 })
+
