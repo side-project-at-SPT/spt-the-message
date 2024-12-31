8b5f33e7a5d0a09fbf61749fd4fe58612a5832e5
sin_yu <catmoon1017@gmail.com>
2025-01-01 01:23:20 +0800

    fix: file name

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,9 +1,15 @@
-const Lobbies = () => {
+import { useNavigate } from 'react-router-dom';
+const Lobby = () => {
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate('/lobby');
+	};
 	return (
 		<>
 			<div className={'text-red-600 text-2xl'}>登入</div>
+			<button onClick={handleClick}>前往大廳</button>
 		</>
 	);
 };
 
-export default Lobbies;
+export default Lobby;
