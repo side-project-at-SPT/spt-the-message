0fa8530d99e7c97891a026d171b2cacdac8eac6b
sin_yu <catmoon1017@gmail.com>
2025-01-01 00:07:30 +0800

    fix: gitignore

---
+++ client/init.sh
@@ -0,0 +1,9 @@
+# Download and install Node.js:
+nvm install 22
+# Verify the Node.js version:
+node -v # Should print "v22.12.0".
+nvm current # Should print "v22.12.0".
+# Download and install pnpm:
+corepack enable pnpm
+# Verify pnpm version:
+pnpm -v
