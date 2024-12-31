e787fb7ebfb7c3a73cea9bbdba827c172088b3c2
sin_yu <catmoon1017@gmail.com>
2025-01-01 01:23:20 +0800

    fix: file name

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 export type RouterConfig = {
-  name: string;
-  path: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  component: any
-}
+	name: string;
+	path: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	component: any;
+};
 
 export const routerMap: RouterConfig[] = [
-  {
-    name: 'login',
-    path: '/',
-    component: React.lazy(() => import("../pages/Login/index"))
-  },
-  {
-    name: 'lobbies',
-    path: '/lobbies',
-    component: React.lazy(() => import("../pages/Lobbies/index"))
-  }
-]
+	{
+		name: 'login',
+		path: '/',
+		component: React.lazy(() => import('../pages/Login/index')),
+	},
+	{
+		name: 'lobby',
+		path: '/lobby',
+		component: React.lazy(() => import('../pages/Lobbies/index')),
+	},
+];
