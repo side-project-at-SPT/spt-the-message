30f9a06a7d92713b2f89abfb832b9c27121bbe1a
noracami <kerke.2011@gmail.com>
2025-01-02 19:00:47 +0800

    wip: login page

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -6,10 +6,10 @@ const Lobby = () => {
   };
 
   return (
-    <>
+    <div className="container max-w-xl mx-auto mt-24">
       <div className={"text-red-600 text-2xl"}>登入</div>
       <button onClick={handleClick}>前往大廳</button>
-    </>
+    </div>
   );
 };
 
