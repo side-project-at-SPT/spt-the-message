939f26ba16985f6fee542837a2ddb2b4756b8af7
noracami <kerke.2011@gmail.com>
2025-01-03 02:09:08 +0800

    chore: add Login page backgroundImage

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import backgroundImage from "../../assets/login.webp";
+
 const Lobby = () => {
   const navigate = useNavigate();
   const handleClick = () => {
@@ -6,10 +8,13 @@ const Lobby = () => {
   };
 
   return (
-    <div className="container max-w-xl mx-auto mt-24">
-      <div className={"text-red-600 text-2xl"}>登入</div>
-      <button onClick={handleClick}>前往大廳</button>
-    </div>
+    <>
+      <img src={backgroundImage} alt="background" className="absolute object-cover min-w-full h-dvh inset-0" />
+      <div className="container max-w-xl mx-auto mt-36 relative">
+        <div className={"text-red-600 text-2xl"}>登入</div>
+        <button onClick={handleClick}>前往大廳</button>
+      </div>
+    </>
   );
 };
 
