385d9d0cc699b502bc6f11ec1dc8c6b3d18fbbc6
noracami <kerke.2011@gmail.com>
2025-01-04 23:11:06 +0800

    wip: improve button style

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -9,14 +9,12 @@ const Lobby = () => {
 
   return (
     <>
-      <img
-        src={backgroundImage}
-        alt="background"
-        className="absolute object-cover min-w-full h-dvh inset-0"
-      />
-      <div className="container max-w-xl mx-auto mt-36 relative">
-        <div className={"text-red-600 text-2xl"}>登入</div>
-        <button onClick={handleClick}>前往大廳</button>
+      <img src={backgroundImage} alt="background" className="absolute object-cover min-w-full h-dvh inset-0" />
+      <div className="container h-dvh  max-w-xl absolute right-40 flex justify-end items-center gap-5">
+        <button className="text-[#982000] from-[#FFFBD6] to-[#FFBA39] bg-gradient-to-b active:translate-y-1 transition-transform duration-300 rounded-xl text-2xl px-7 py-2.5 w-fit text-center">前往大廳</button>
+        <button className="text-[#982000] from-[#FFFBD6] to-[#FFBA39] bg-gradient-to-b active:translate-y-1 transition-transform duration-300 shadow-[0_0px_60px_0px_rgba(255,146,0,1)] rounded-xl text-2xl px-7 py-2.5 w-fit text-center">前往大廳</button>
+        <button className="text-white rounded-xl text-2xl p-2 w-fit text-center bg-cyan-700" onClick={handleClick}>前往大廳</button>
+        <div className={"text-white rounded-xl text-2xl p-2 w-fit text-center bg-cyan-700"}>登入</div>
       </div>
     </>
   );
